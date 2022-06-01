63e3eef5b839a025bc54806fd75718db44e1a3fb
Naime Medjek <naimemedjek@gmail.com>
2022-06-01 14:52:01 +0200

    points hardcoded

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let player = {
+    name: "Naime",
+    points: 10
+}
+let sum = 0
+let cards = []
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + " : " + player.points + " points"
+function randomCard() {
+    let randomNum = Math.floor( Math.random()*13 ) + 1
+
+    if (randomNum >10) {
+        return 10 
+    } else if ( randomNum === 1) {
+            return 11   
+    } else { 
+    return randomNum
+    }
+} 
+
 
 function startGame() {
-    renderGame()
+
+isAlive = true
+let firstCard = randomCard()
+let secondCard = randomCard()
+cards = [firstCard, secondCard]
+sum = firstCard + secondCard
+renderGame()
 }
 
 function renderGame() {
+
+cardsEl.textContent = "Cards: "
+
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
     sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+     
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -29,8 +59,12 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = 3
-    sum += card
-    cards.push(card)
-    renderGame()
+    if(isAlive === true && hasBlackJack === false) {
+        
+        let card = randomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+
+    }
 }
